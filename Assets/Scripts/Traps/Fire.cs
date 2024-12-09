17012b1fef9ce3d0eb0ed3df2b0d15105f5e7472
Currry218 <chickennoodlesoup218@gmail.com>
2024-12-09 18:55:43 +0700

    Add Saw & Fire script

--- Assets/Scripts/Traps/Fire.cs
+++ Assets/Scripts/Traps/Fire.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _offDuration;
+    [SerializeField] private FireButton _fireButton;
+
+    private Animator _animator;
+    private CapsuleCollider2D _capsuleCollider;
+    private bool _isActive;
+    private void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
+        _capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if (_fireButton == null)
+            Debug.LogWarning("No fire button on " + gameObject.name);
+
+        SetFire(true);
+    }
+
+    private IEnumerator FireCoroutine()
+    {
+        SetFire(false);
+        yield return new WaitForSeconds(_offDuration);
+        SetFire(true);
+    }
+
+    private void SetFire(bool active)
+    {
+        _animator.SetBool("isActive", active);
+        _capsuleCollider.enabled = active;
+        _isActive = active;
+    }
+
+    public void SwitchOffFire()
+    {
+        if (!_isActive)
+            return;
+            
+        StartCoroutine(FireCoroutine());
     }
 }
